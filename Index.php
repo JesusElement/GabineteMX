edc54f2e01d394aa21761d5ee5d752d8f4593471
root <jesus.e.b.h@comunidad.unam.mx>
2020-03-27 21:03:50 -0600

    PhpPrueba

--- Index.php
+++ Index.php
@@ -1,96 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- Compiled and minified CSS -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
-    <link rel="stylesheet" href="/css/Style.css">
-    <script src="https://unpkg.com/ionicons@5.0.0/dist/ionicons.js"></script>
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-      <!-- Compiled and minified JavaScript -->
-      <script src="https://unpkg.com/vue"></script>
-      <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
-      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-      <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
     <title>Gabinete MX</title>
+    <?php include('Componentes/Head.php') ?>
 </head>
 <body>
     <div class="contenedor">
         <div class="header">
-            <!-- Haeader barra sub INICIO-->
-            <div class="barsub">
-                <div class="logoDiv">
-                  <a href="Index.html">
-                    <img class="logo" src="/Imagenes/logo.png" alt="GabineteMX">
-                  </a>
-                </div>
-                <div class="menuDiv">
-                    <a class="dropdown-trigger btnMenu" href='#' data-target='dropdown1'><i class="small material-icons">toc</i></a>
-                    <ul id='dropdown1' class='dropdown-content dropMenu'>
-                        <li><a class="grey-text  text-lighten-5" href="#!">Pedidos</a></li>
-                        <li><a class="grey-text  text-lighten-5" style="font-size: 15px;" href="#!">Devoluciones</a></li>
-                        <li class="divider" tabindex="-1"></li>
-                        <li><a class="grey-text  text-lighten-5" href="#!">Cuenta</a></li>
-                        <li><a class="grey-text  text-lighten-5" style="font-size: 15px;" href="#!">Compras</a></li>
-                        <li><a class="grey-text  text-lighten-5" style="font-size: 15px;" href="#!">Dirección</a></li>
-                      </ul>
-                </div>
-                <div class="buscarDiv">
-                    <nav  class="buscador">
-                        <div class="nav-wrapper">
-                          <form>
-                            <div class="input-field">
-                              <input class="buscardor grey-text text-darken-4" id="search" type="search">
-                              <label class="label-icon" for="search"><i class="material-icons">search</i></label>
-                            </div>
-                          </form>
-                        </div>
-                    </nav>
-                </div>
-                <div class="fotoperfilDiv">
-                    <i class="small material-icons">account_box</i>  
-                </div>
-                <div class="nombreperfilDiv">
-                    <a href="Login.html" class="waves-effect waves-light btn btnIng">Ingresar</a>
-                </div>
-                <div class="ubicacionDiv">
-                    <p style="font-size: 10px;">Ubicación</p>
-                    <p style="display: inline;">Cuautitlán</p>
-                    <i class="material-icons">place</i>
-                </div>
-                <div class="carritoDiv">
-                    <i class="small material-icons">add_shopping_cart</i>
-                </div>
-                  
-            </div>
-            <!-- Haeader barra sub FIN-->
-          <!-- Haeader barra inf INICIO-->
-          <div class="barinf">
-                <div class="ejecutivaDiv">
-                    Ejecutiva  
-                </div>
-                <div class="trabajoDiv">
-                    Trabajo   
-                </div>
-                <div class="gamerDiv">
-                    Gamer   
-                </div>
-                <div class="hardwareDiv">
-                    Hardware     
-                </div>
-                <div class="gadetsDiv">
-                    Gadgets   
-                </div>
-                <div class="masbuscadoDiv">
-                    Lo mas buscado   
-                </div>
-                <div class="mascalificadoDiv">
-                    Lo mejor calificado   
-                </div>
-          </div>
-        <!-- Haeader barra inf FIN-->
+          <?php include('Componentes/Heder.php') ?>
         </div>
         <!-- Inicia Contenido -->
         <div class="contenido">
