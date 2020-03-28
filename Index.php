7093265c1144dd6957674a4ee7d37202ce548097
root <jesus.e.b.h@comunidad.unam.mx>
2020-03-27 21:45:43 -0600

    pollos

--- Index.php
+++ Index.php
@@ -2,12 +2,12 @@
 <html lang="en">
 <head>
     <title>Gabinete MX</title>
-    <?php include('Componentes/Head.php') ?>
+    <?php include('Componentes/Head.php'); ?>
 </head>
 <body>
     <div class="contenedor">
         <div class="header">
-          <?php include('Componentes/Heder.php') ?>
+          <?php include('Componentes/Heder.php'); ?>
         </div>
         <!-- Inicia Contenido -->
         <div class="contenido">
@@ -134,35 +134,7 @@
         </div>
         <!-- Termina contenido -->
         <div class="footer">
-              <footer class="page-footer footerCcc">
-          <div class="container">
-            <div class="row">
-              <div class="col l6 s12">
-                <h5 class="white-text">¿Quienes somos?</h5>
-                <p class="grey-text text-lighten-4">
-                    Nuestros productos están enfocados para expertos, aficionados de tecnologias, asi como, empresas que se encuentran en el ramo de las TI.
-
-                        
-                </p>
-              </div>
-              <div class="col l4 offset-l2 s12">
-                <h5 class="white-text">Ayuda</h5>
-                <ul>
-                  <li><a class="grey-text text-lighten-3" href="#!">Servicio al cliente</a></li>
-                  <li><a class="grey-text text-lighten-3" href="#!">Contacto</a></li>
-                  <li><a class="grey-text text-lighten-3" href="#!">Rastrear</a></li>
-                  <li><a class="grey-text text-lighten-3" href="#!">Informción corporativa</a></li>
-                </ul>
-              </div>
-            </div>
-          </div>
-          <div class="footer-copyright">
-            <div class="container">
-                    Aviso de privacidad
-            <a class="grey-text text-lighten-4 right" href="#!">Terminos y codiciones</a>
-            </div>
-          </div>
-        </footer>
+    <?php include('Componentes/Footer.php'); ?>
             
         </div>
     </div>
