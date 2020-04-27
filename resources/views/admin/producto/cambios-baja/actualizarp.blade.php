f38d9dab4d4db0a332844fa48bde9b6139a27425
AntonioHernandezO <hxckredx@gmail.com>
2020-04-27 14:21:51 -0500

    Correccion form/redirect

--- resources/views/admin/producto/cambios-baja/actualizarp.blade.php
+++ resources/views/admin/producto/cambios-baja/actualizarp.blade.php
@@ -5,49 +5,44 @@
   <div class="actualizarProductoCss">
     <div class="actProTabCss">
       <h3>DASHBOARD</h3>
-    <table style="">
+      <table style="">
         <tr>
-          <th>Tipo</th>     {{--   id_familia --}}
-          <th>Subtipo</th>  {{--   clav_clas --}}
-          <th>Provedor</th>   {{--   id_provee --}}
-          <th>Nombre</th>      {{--   titulo --}}
-          <th>Descripcion</th>      {{--   datos --}}
-          <th>Opciones</th>      {{-- actualizarEliminar --}}
-       
+          <th>Tipo</th> {{--   id_familia --}}
+          <th>Subtipo</th> {{--   clav_clas --}}
+          <th>Provedor</th> {{--   id_provee --}}
+          <th>Nombre</th> {{--   titulo --}}
+          <th>Descripcion</th> {{--   datos --}}
+          <th>Opciones</th> {{-- actualizarEliminar --}}
+
 
         </tr>
-       
-          @foreach($resultado ?? '' as $resultados)
-          <tr>
+
+        @foreach($resultado ?? '' as $resultados)
+        <tr>
           <td>{{ $resultados->nom_fami}}</td>
           <td>{{ $resultados->name}}</td>
           <td>{{ $resultados->nom}}</td>
           <td>{{ $resultados->titutlo}}</td>
           <td>{{ $resultados->datos}}</td>
-          <td style="width: 170px;"><a class="waves-effect waves-light btn blue darken-4"><i class="small material-icons left">arrow_upward</i>Actualizar</a><a class="waves-effect waves-light btn red accent-4 btnacP"><i class="small material-icons left">warning</i>Eliminar</a></td>
-          
+          <td style="width: 170px;"><a class="waves-effect waves-light btn blue darken-4"><i
+                class="small material-icons left">arrow_upward</i>Actualizar</a><a
+              class="waves-effect waves-light btn red accent-4 btnacP"><i
+                class="small material-icons left">warning</i>Eliminar</a></td>
+
         </tr>
         @endforeach
 
-       
 
-        
+
+
       </table>
 
-      </div>
+    </div>
+    <br>
 
-      <br>
-      <br>
-      <br>
-      <br>
-      <br>
 
-      <br>
-      
-      
-      
-      </div>
-      
+  </div>
+
 </div>
 <!-- Termina contenido -->
 @endsection
