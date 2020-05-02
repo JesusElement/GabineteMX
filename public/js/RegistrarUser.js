8b02df8f82254b28cc7696afcc6c180f2f783b27
daveimpala43-2 <david_jhernandez@outlook.com>
2020-05-01 22:49:08 -0500

    Se corrigio el error del registro

--- public/js/RegistrarUser.js
+++ public/js/RegistrarUser.js
@@ -1,37 +1,37 @@
-$(document).ready(function () {
-    
+$(document).ready(function() {
 
-$("#blockDosRegistroUserCss").css("display","none");
-$("#blockTresRegistroUserCss").css("display","none");
+
+    $("#blockDosRegistroUserCss").css("display", "none");
+    $("#blockTresRegistroUserCss").css("display", "none");
     //   var covit  =$("#blockDosRegistroUserCss").css("display");
     //     window.alert(covit);
-  $('#siguienteDos').click(function () {
-   
-   
-    $("#blockUnoRegistroUserCss").css("display","none");
-    $("#blockDosRegistroUserCss").css("display","grid");
-  });
-
-  $('#RegresaUno').click(function () {
-   
-    $("#blockDosRegistroUserCss").css("display","none");
-    $("#blockUnoRegistroUserCss").css("display","grid");
-  
-  });
+    $('#siguienteDos').click(function() {
 
 
-  $('#RegresaDos').click(function () {
-   
-    $("#blockTresRegistroUserCss").css("display","none");
-    $("#blockDosRegistroUserCss").css("display","grid");
-  
-  });
+        $("#blockUnoRegistroUserCss").css("display", "none");
+        $("#blockDosRegistroUserCss").css("display", "grid");
+    });
 
-  $('#siguienteTres').click(function () {
-   
-   
-    $("#blockDosRegistroUserCss").css("display","none");
-    $("#blockTresRegistroUserCss").css("display","grid");
-  });
+    $('#RegresaUno').click(function() {
+
+        $("#blockDosRegistroUserCss").css("display", "none");
+        $("#blockUnoRegistroUserCss").css("display", "grid");
+
+    });
+
+
+    $('#RegresaDos').click(function() {
+
+        $("#blockTresRegistroUserCss").css("display", "none");
+        $("#blockDosRegistroUserCss").css("display", "grid");
+
+    });
+
+    $('#siguienteTres').click(function() {
+
+
+        $("#blockDosRegistroUserCss").css("display", "none");
+        $("#blockTresRegistroUserCss").css("display", "grid");
+    });
 
 });
