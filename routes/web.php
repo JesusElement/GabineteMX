7633535ee043e0aa5b8e52f91f2b1c3610bfc3de
daveimpala43-2 <david_jhernandez@outlook.com>
2020-05-06 16:41:44 -0500

    ; que se me paso xd

--- routes/web.php
+++ routes/web.php
@@ -77,7 +77,7 @@ Route::get('/buscarproducto', function() {
     Route::get('admin/home', 'HomeController@adminHome')->name('admin.index')->middleware('is_admin');
 
     Route::get('/home', 'HomeController@index')->name('home');
-    Route::get('/altaproducto', 'ProductoController@index')->name('altaproducto')
+    Route::get('/altaproducto', 'ProductoController@index')->name('altaproducto');
     Route::get('/actualizarproducto', 'ProductoController@show')->name('verproducto');
     Route::post('/storeproducto', 'ProductoController@store')->name('insertarproducto');
     Route::delete('/actualizarproducto/{id_produc}', 'ProductoController@destroy')->name('eliminarproducto');
