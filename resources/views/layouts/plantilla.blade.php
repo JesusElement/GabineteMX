6d460683972fffbd577896a48fd9ee2464a249e1
daveimpala43-2 <david_jhernandez@outlook.com>
2020-05-07 17:05:40 -0500

    privilegios de admin y se agrego chat bot

--- resources/views/layouts/plantilla.blade.php
+++ resources/views/layouts/plantilla.blade.php
@@ -68,9 +68,12 @@
                     </nav>
                 </div>
                 <div class="fotoperfilDiv">
-                    <i class="small material-icons">account_box</i>  
+                  <a class='dropdown-trigger colortext' data-target='dropdown3'>  <i class="small material-icons" >account_box</i>  </a>
+                  <ul id='dropdown3' class='dropdown-content dropmenu'>
+                    <li class="blue-text text-darken-2"></li>
+                  </ul>
+                    
                 </div>
-                {{auth()->user()}}
                 @guest
                 <div class="nombreperfilDiv">
                     <a href="{{route('login')}}" class="waves-effect waves-light btn btnIng">Ingresar</a>
@@ -183,6 +186,7 @@
 
     </div>
   </div>
+  <script> (function(b,c){var e=document.createElement('link');e.rel='stylesheet',e.type='text/css',e.href='https://chatboxlive.blahbox.net/static/css/main.css',document.getElementsByTagName('head')[0].appendChild(e); var f=document.createElement('script');f.onload=function(){var g;if(c)g='previewInit';else{var h=document.createElement('div');g='cbinit',h.id='cbinit',document.body.append(h)} console.log(document.querySelector('#'+g)),chatbox.initChat(document.querySelector('#'+g),b,c)},f.src='https://chatboxlive.blahbox.net/static/js/chat-lib.js',document.getElementsByTagName('head')[0].appendChild(f)}) ('67fdf26bb08d7701422d77bc25cfa268', 0); </script>
 </body>
 
 </html>
