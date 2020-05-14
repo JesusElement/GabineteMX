c76c7f8383ff4ac31e91e0f6f5ae03df95a32754
daveimpala43-2 <david_jhernandez@outlook.com>
2020-05-14 14:55:59 -0500

    se agreso Aviso y Termino a la pagina

--- resources/views/cliente/ayuda/index.blade.php
+++ resources/views/cliente/ayuda/index.blade.php
@@ -4,12 +4,17 @@
 
 
     <div class="ayuda">
+        <div class="modifica">
+            <p>Ultima Modificacion:</p>
+            <p>6-05-2020</p>
+        </div>
 
         <div class="opciones">
         <a class="btn btn-block">Servicio al cliente</a>
         <a class="btn btn-block">Contacto</a>
         <a class="btn btn-block">Información Corporativas</a>
         <a class="btn btn-block" onclick="aviso()">Aviso de Privacidad</a>
+        <a class="btn btn-block" onclick="termi()">Terminos y Condiciones</a>
         </div>
 
         <div class="informacion" id="content">
@@ -20,7 +25,18 @@
     <script>
         var contenido = document.querySelector('#content');
         function aviso(){
-            fetch('contenidos/Aviso_Privacidad.txt')
+            fetch('contenidos/Aviso_Privacidad.html')
+            .then(data => data.text())
+                //Otra promesa que captura esa data que ya viene preformateada
+                .then(data => {
+                    //pintamos la data
+                    contenido.innerHTML = data
+
+                })
+
+        }
+        function termi(){
+            fetch('contenidos/Ter_Condi.html')
             .then(data => data.text())
                 //Otra promesa que captura esa data que ya viene preformateada
                 .then(data => {
