4994e3a75cd3623aadd4bd8e8bc13fd470bb7bc1
JesusElement <jesus.e.b.h@comunidad.unam.mx>
2020-05-21 10:34:00 -0500

    PagoInicio

--- resources/views/cliente/pago/index.blade.php
+++ resources/views/cliente/pago/index.blade.php
@@ -5,6 +5,7 @@
         max-height: 100%;
         height: 100%;
         top: 0 !important;
+        background-color: #f0f0f0;
     }
 </style>
 
@@ -12,37 +13,98 @@
     {{-- INICIA MODAL DE UPDATE --}}
     <div class="modal-content"> {{-- -INICIA CONTENT --}}
         <div class="modal-body"> {{-- INICIA BODY --}}
-
-
-            <aside class="contenedor60"> {{-- INICIA EL 60 % DEL FORMULARIO --}}
-                <div class="contenido">
-                    <h4>Contenedor de arriba</h4>
-
-
-                    <div class="bttnCss">
-                        <button class="modal-action modal-close waves-effect waves-red btn red lighten-1"
-                            id="cerrarmodal">Cerrar</button>
-                    </div>
-                    <div class="marder">
-
-                    </div>
-
-                    
+            <div class="contenedor61"> {{-- INICIA EL 60 % DEL FORMULARIO --}}
+                <div class="titulo">
+                    <h4>Pago</h4>
                 </div>
+                <div class="HederTres">
+                    <div class="Pan1">
+                        <h5>Dirección</h5>
+                        <div class="input-field col s12">
+                            <select>
+                                <option value="" disabled selected>Direcciones</option>
+                                <option value="1">Option 1</option>
+                                <option value="2">Option 2</option>
+                                <option value="3">Option 3</option>
+                            </select>
+                            <label>Dirección de envio</label>
+                        </div>
+                    </div>
+                    <div class="Pan2">
+                        <h5>Tarjeta</h5>
+                        <div class="input-field col s12">
+                            <select>
+                                <option value="" disabled selected>Tarjetas</option>
+                                <option value="1">Option 1</option>
+                                <option value="2">Option 2</option>
+                                <option value="3">Option 3</option>
+                            </select>
+                            <label>Selecciona tarjeta</label>
+                        </div>
+                    </div>
+                    <div class="Pan3">
+                        <div class="col s12 m7">
+                            <h5 class="header">Envio gratis</h5>
+                            <div class="card horizontal c">
+                                <i class="material-icons i">a card_giftcard</i>
+                                <div class="card-stacked c">
+                                    <div class="card-content c">
+                                        <p>Por inauguración temporalmente todos nuestros envios son gratis</p>
+                                    </div>
+                                    <div class="card-action">
+                                        <a href="#">This is a link</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
 
-            </aside> {{-- TERMINA 60% --}}
+                    </div>
 
+                </div>
+            <div class="contenedor21">
+                @foreach ($Carrito as $q)
+                <div class="carritoCardCss z-depth-2">
+                    <div class="carritoCardImgCss">
+                        <img class="carritoImgCss"
+                            src="/Imagenes/Productos/{{ $q->nom_fami }}/{{ $q->nom }}/{{ $q->id_produc }}/1.jpg"
+                            alt="">
+                    </div>
+                    <div class="carritoCardInfoCss">
+                        <h5 class="titulo">{{ $q->titulo }}</h5>
+                        <p>{{ $q->nom }}</p>
+                        <p>Cantidad:</p>
+                        <p style="display: none;">{{ $H = $q->stock }}</p>
+                        <a class="waves-effect waves-light btn transparent"
+                            href="{{ url("carritoMenos/{$q->id_produc}") }}"
+                            id="quantity"><i class="b material-icons">remove</i></a>
+                        <b style="margin: 1rem; background-color: #fff;">{{ $q->cantidad }}</b>
+                        <a class="waves-effect waves-light btn transparent"
+                            href="{{ url("carritoMas/{$q->id_produc}") }}" id="quantity"><i
+                                class="b material-icons">add</i></a>
+                    </div>
+                    <div class="carritoEliPreCss">
+                        <b class="precio">${{ number_format($q->cantidad*$q->prec_uni,2) }} x
+                            {{ $q->cantidad }}</b>
+                        <a class="Eli"
+                            href="{{ url("EliCarrito/{$q->id_produc}") }}">Eliminar</a>
 
+                    </div>
 
-        <article class="contenedor40"> {{-- INICIA 40% PARA MOSTRAR IMAGENES DEL PRODUCTO --}}
-            <div> {{-- INICIA TITUTLO 40% --}}
-                <h4>Contenedor abajo</h4>
-            </div> {{-- TERMINA TITUTLO 40% --}}
-
-
-
-        </article>{{-- TERMINA 40% PARA MOSTRAR IMAGENES DEL PRODUCTO --}}
-
+                </div>
+                @php
+                $Total = $Total + $q->prec_uni * $q->cantidad;
+            @endphp
+              @endforeach
+      
+            </div>
+            <div class="Pago">
+                Aqui se pagan tus mamadas
+                <div class="bttnCss">
+                    <button class="modal-action modal-close waves-effect waves-red btn red lighten-1"
+                        id="cerrarmodal">Cerrar</button>
+                </div>
+            </div>
+            </div>
         </div> {{-- TERMINA BODY --}}
     </div> {{-- TERMINA CONTENT --}}
 </div>{{-- TERMINA MODAL --}}
