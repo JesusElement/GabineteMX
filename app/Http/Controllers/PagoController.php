37c1bd483768ab288e4209f1b645631c7985139e
AntonioHernandezO <hxckredx@gmail.com>
2020-05-21 08:44:01 -0500

    Modalprocederpago

---
+++ app/Http/Controllers/PagoController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Pago;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class PagoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Pago  $pago
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Pago $pago)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Pago  $pago
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Pago $pago)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Pago  $pago
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Pago $pago)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Pago  $pago
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Pago $pago)
+    {
+        //
+    }
+}
---
+++ resources/views/cliente/pago/index.blade.php
@@ -0,0 +1,48 @@
+<style>
+    /* .modal { max-height: 100%; overflow: visible;} */
+    .fullscreen.open {
+        width: 100%;
+        max-height: 100%;
+        height: 100%;
+        top: 0 !important;
+    }
+</style>
+
+<div class="modal fullscreen" id="#procederpago">
+    {{-- INICIA MODAL DE UPDATE --}}
+    <div class="modal-content"> {{-- -INICIA CONTENT --}}
+        <div class="modal-body"> {{-- INICIA BODY --}}
+
+
+            <aside class="contenedor60"> {{-- INICIA EL 60 % DEL FORMULARIO --}}
+                <div class="contenido">
+                    <h4>Contenedor de arriba</h4>
+
+
+                    <div class="bttnCss">
+                        <button class="modal-action modal-close waves-effect waves-red btn red lighten-1"
+                            id="cerrarmodal">Cerrar</button>
+                    </div>
+                    <div class="marder">
+
+                    </div>
+
+                    
+                </div>
+
+            </aside> {{-- TERMINA 60% --}}
+
+
+
+        <article class="contenedor40"> {{-- INICIA 40% PARA MOSTRAR IMAGENES DEL PRODUCTO --}}
+            <div> {{-- INICIA TITUTLO 40% --}}
+                <h4>Contenedor abajo</h4>
+            </div> {{-- TERMINA TITUTLO 40% --}}
+
+
+
+        </article>{{-- TERMINA 40% PARA MOSTRAR IMAGENES DEL PRODUCTO --}}
+
+        </div> {{-- TERMINA BODY --}}
+    </div> {{-- TERMINA CONTENT --}}
+</div>{{-- TERMINA MODAL --}}
